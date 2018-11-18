d9d87f0545c26745605863b7f7e0833bcb11cafd
MrValdez <shunyvaldez@gmail.com>
2018-11-18 16:45:32 +0800

    fixing the movement artifacts

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ pygame.init()
 screen = pygame.display.set_mode(resolution)
 clock = pygame.time.Clock()
 
-hero = pygame.image.load("hero.png")
+hero = pygame.image.load("hero.png").convert()
+hero.set_colorkey((255, 128, 128))
 hero_pos = [0, 0]
 
 while isRunning:
+    screen.fill((255, 255, 255))
     tick = clock.tick(60)
 
     for event in pygame.event.get():
