b670708da65bf21b5b2a44229419f7afd03d6f00
MrValdez <shunyvaldez@gmail.com>
2018-11-18 16:23:48 +0800

    Moving the sprite

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ isRunning = True
 
 pygame.init()
 screen = pygame.display.set_mode(resolution)
+clock = pygame.time.Clock()
 
 hero = pygame.image.load("hero.png")
 hero_pos = [0, 0]
 
 while isRunning:
+    tick = clock.tick(60)
+
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
@@ -17,6 +20,9 @@ while isRunning:
 
         if event.type == pygame.QUIT:
             isRunning  = False
+
+    hero_pos[0] += 10
+    hero_pos[1] += 10
 
     screen.blit(hero, hero_pos)
 
